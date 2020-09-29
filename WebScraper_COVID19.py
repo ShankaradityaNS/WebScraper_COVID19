9f6eccbe6ece9198e0f8e08c6160659ae0479129
Shankaraditya N S <50665969+ShankaradityaNS@users.noreply.github.com>
2020-09-29 22:38:10 +0530

    Create WebScraper_COVID19.py

---
+++ WebScraper_COVID19.py
@@ -0,0 +1,17 @@
+#Importing required libraries
+import requests
+from bs4 import BeautifulSoup
+
+# You can also get data for specific countries by changing the URL
+# For example
+# for US: https://www.worldometers.info/coronavirus/country/us/
+# for India: https://www.worldometers.info/coronavirus/country/india/
+
+url = "https://www.worldometers.info/coronavirus/"
+req = requests.get(url)
+bsObj = BeautifulSoup(req.text, "html.parser")
+data = bsObj.find_all("div",class_ = "maincounter-number")
+
+print("Total Cases: ", data[0].text.strip())
+print("Total Deaths: ", data[1].text.strip())
+print("Total Recovered: ", data[2].text.strip())
